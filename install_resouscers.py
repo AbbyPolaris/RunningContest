0cbd1b0d9984e158b4904445a72c91672601fbc3
Abby <northern-crown@outlook.com>
2024-10-06 22:02:20 +0330

    add storage folder.

--- install_resouscers.py
+++ install_resouscers.py
@@ -4,4 +4,4 @@ modules = ['pyserial','pyqtdarktheme','pyside6==6.4.1','screeninfo']
 for module in modules:
     returned_value = os.system('pip install '+module)
     print('returned value:', returned_value)
-    
+print(os.system('mkdir storage'))
